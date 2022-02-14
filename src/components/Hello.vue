3e1a30cbad30dbed89bf31f77699fedc403a0a9c
Kevin Le Van Phung <levanphungk@gmail.com>
2022-02-14 11:22:27 +0100

    setting clean project
    renamung HelloWorld.vue and wiping clean the project

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -9,14 +9,9 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
   
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
+<style lang ="scss">
+
 </style>
